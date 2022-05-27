c3e9c92e05edaec4d9e439217d607563dca64566
AgEwa <100518836+AgEwa@users.noreply.github.com>
2022-05-28 01:10:54 +0200

    Add files via upload

--- PDU_Projekt3/app.R
+++ PDU_Projekt3/app.R
@@ -12,18 +12,30 @@ source("helpers.R")
 ui <- fluidPage(
   titlePanel("Zapytanie"),
   
+  # rozpisany wygląd aplikacji
+  
   sidebarLayout(
     sidebarPanel(
-      helpText("Realizuje zapytanie"),
+      helpText("Realizuje zapytanie, dni: 0- 1 maja, 91 - 31 lipca"),
       
       selectInput("var", 
                   label = "Choose a city to display",
                   choices = c("New York", "New Jersy"),
-                  selected = "New York"),
+                  selected = "New Jesrsy"),
       
-      sliderInput("range", 
-                  label = "Range of interest of years of birth:",
-                  min = 1899, max = 1998, value = c(1950, 1980))
+      dateRangeInput("range",
+                     label = "Date range:",
+                     start  = "2016-05-13",
+                     end    = "2016-06-05",
+                     min    = "2016-05-01",
+                     max    = "2016-07-31",
+                     format = "mm/dd/yy",
+                     separator = " - "),
+      
+      
+      #sliderInput("range", 
+       #           label = "Range of interest of days:",
+        #          min = 0, max = 91, value = c(20,40))
     ),
     
     mainPanel(plotOutput("map"))
@@ -31,13 +43,14 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+  # rozpisane działanie aplikacji, wywołuje skrypt z zapytaniem i wykresem
   output$map <- renderPlot({
     NYC <- switch(input$var,
                    "New York" = T,
                    "New Jersy" = F)
     
-    #args$min <- input$range[1]
-    #args$max <- input$range[2]
+    #args$min <- input$datarange3[1]
+    #args$max <- input$datarange3[2]
     
     do.call(getThePlot, list(NYC,input$range[1], input$range[2]))
   })
